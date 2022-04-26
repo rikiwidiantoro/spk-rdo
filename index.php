bb906729a7fc18f80a2db149419599479e6fa0c4
Riki-Toro <rikitoro22@gmail.com>
2022-04-26 16:21:07 +0700

    read tabel kriteria

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!-- koneksi -->
+<?php
+    include_once('koneksi.php');
+
+    $kriterias = mysqli_query($koneksi, "SELECT * FROM kriteria");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,11 +16,15 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-    <!-- data table -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <!-- css sendiri -->
+    <style>
+        .kriteria .tambah-kriteria {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -32,40 +44,48 @@
 
     <!-- tabel kriteria -->
     <br><br>
-    <div id="kriteria">
+    <div id="kriteria" class="kriteria">
         <div class="container">
             <div class="row">
-                <div class="col">
+                <div class="col m9">
                     <h5>Tabel Kriteria</h5>
+                </div>
+                <div class="col m3">
+                    <a class="waves-effect waves-light btn-small grey darken-1 tambah-kriteria"><i class="material-icons left">add</i>Tambah Kriteria</a>
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                <table id="table_kriteria" class="display" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>Kriteria</th>
-                            <th>Nama Kriteria</th>
-                            <th>Cost/Benefit</th>
-                            <th>Bobot</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>C1</td>
-                            <td>Manajer Investasi</td>
-                            <td>Benefit</td>
-                            <td>0.2</td>
-                        </tr>
-                        <tr>
-                            <td>C2</td>
-                            <td>Total AUM</td>
-                            <td>Benefit</td>
-                            <td>0.2</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="col m12 s12">
+                    <table class="responsive-table" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Kriteria</th>
+                                <th>Nama Kriteria</th>
+                                <th>Cost/Benefit</th>
+                                <th>Bobot</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                foreach($kriterias as $kriteria) {
+                                    echo "
+                                    <tr>
+                                        <td>". $kriteria['no_kriteria'] ."</td>
+                                        <td>". $kriteria['nama_kriteria'] ."</td>
+                                        <td>". $kriteria['cost_benefit'] ."</td>
+                                        <td>". $kriteria['bobot_kriteria']."</td>
+                                        <td>
+                                            <a class='waves-effect waves-light btn-small grey darken-1'><i class='material-icons left'>create</i>Edit</a>
+                                            <a class='waves-effect waves-light btn-small grey darken-1'><i class='material-icons left'>delete</i>Hapus</a>
+                                        </td>
+                                    </tr>
+                                    
+                                    ";
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -81,19 +101,6 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-    <!-- data table -->
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
-    
-    <script>
-        $(document).ready(function() {
-            $('#table_kriteria').DataTable( {
-                "paging":   false,
-                "ordering": false,
-                "info":     false
-            } );
-        } );
-    </script>
 
 </body>
 </html>
---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+    $localhost = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "db_saw_rdo";
+
+    $koneksi = mysqli_connect($localhost, $username, $password, $db_name);
+
+?>
