114f267d9038e41b9bb40ae4c1e2f5b4a0b21d34
Riki-Toro <rikitoro22@gmail.com>
2022-04-28 23:26:17 +0700

    selesai matriks X

--- hasil_perangkingan.php
+++ hasil_perangkingan.php
@@ -20,11 +20,9 @@
 
     <!-- css sendiri -->
     <style>
-        .kriteria .tambah-kriteria {
-            margin-top: 20px;
-        }
-        .kriteria table thead th {
-            text-align: center;
+        footer {
+            height: 70px;
+            line-height: 70px;
         }
     </style>
 </head>
@@ -45,6 +43,152 @@
 
     <!-- tombol lihat hasil -->
     <br><br>
+    <div class="lihat-saw">
+        <div class="container">
+            <div class="row">
+                <div class="col m12 center">
+                    <a class="waves-effect waves-light btn-small grey darken-1 tambah-kriteria"><i class="material-icons left">keyboard_arrow_down</i>Lihat SAW</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- tombol lihat hasil -->
+
+    <!-- matriks X -->
+    <div class="matriksX">
+        <div class="container">
+            <div class="row">
+                <div class="col m12">
+                    <h5>Tabel Matriks X</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col m12">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Alternatif</th>
+                                <th>Manajer Investasi</th>
+                                <th>C1</th>
+                                <th>C2</th>
+                                <th>C3</th>
+                                <th>C4</th>
+                                <th>C5</th>
+                                <th>C6</th>
+                                <th>C7</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                foreach($alternatifs as $alternatif) {
+                                    // kriteria1 = manajer investasi = x1
+                                    if( $alternatif['kriteria1'] == "Sucor Asset Management" || $alternatif['kriteria1'] == "Trimegah Asset Management" ) {
+                                        $x1 = 5;
+                                    } else if( $alternatif['kriteria1'] == "Asset Management Sinarmas" || $alternatif['kriteria1'] == "Eastpring Investments" ) {
+                                        $x1 = 4;
+                                    } else {
+                                        $x1 = 3;
+                                    }
+            
+                                    // kriteria2 = total AUM = x2
+                                    if( $alternatif['kriteria2'] > 1 ) {
+                                        $x2 = 5;
+                                    } else if( $alternatif['kriteria2'] <= 1 ) {
+                                        $x2 = 4;
+                                    } else {
+                                        $x2 = 3;
+                                    }
+            
+                                    // kriteria3 = CAGR 1 tahun = x3
+                                    if( $alternatif['kriteria3'] > 8 ) {
+                                        $x3 = 5;
+                                    } else if( $alternatif['kriteria3'] >= 6.01 && $alternatif['kriteria3'] <= 8 ) {
+                                        $x3 = 4;
+                                    } else if( $alternatif['kriteria3'] >= 5.01 && $alternatif['kriteria3'] <= 6 ) {
+                                        $x3 = 3;
+                                    } else if( $alternatif['kriteria3'] >= 3 && $alternatif['kriteria3'] <= 5 ) {
+                                        $x3 = 2;
+                                    } else if( $alternatif['kriteria3'] < 3 ) {
+                                        $x3 = 1;
+                                    }
+            
+                                    // kriteria4 = dropdown 1 tahun = x4
+                                    if( $alternatif['kriteria4'] > 5 ) {
+                                        $x4 = 1;
+                                    } else if( $alternatif['kriteria4'] >= 3.01 && $alternatif['kriteria4'] <= 5 ) {
+                                        $x4 = 2;
+                                    } else if( $alternatif['kriteria4'] >= 2.01 && $alternatif['kriteria4'] <= 3 ) {
+                                        $x4 = 3;
+                                    } else if( $alternatif['kriteria4'] >= 1 && $alternatif['kriteria4'] <= 2 ) {
+                                        $x4 = 4;
+                                    } else if( $alternatif['kriteria4'] < 1 ) {
+                                        $x4 = 5;
+                                    }
+            
+                                    // kriteria5 = expense ratio = x5
+                                    if( $alternatif['kriteria5'] > 2 ) {
+                                        $x5 = 1;
+                                    } else if( $alternatif['kriteria5'] >= 1.51 && $alternatif['kriteria5'] <= 2 ) {
+                                        $x5 = 2;
+                                    } else if( $alternatif['kriteria5'] >= 1.01 && $alternatif['kriteria5'] <= 1.5 ) {
+                                        $x5 = 3;
+                                    } else if( $alternatif['kriteria5'] >= 0.5 && $alternatif['kriteria5'] <= 1 ) {
+                                        $x5 = 4;
+                                    } else if( $alternatif['kriteria5'] < 0.5 ) {
+                                        $x5 = 5;
+                                    }
+            
+                                    // kriteria6 = minimal pembelian = x6
+                                    if( $alternatif['kriteria6'] > 5000000 ) {
+                                        $x6 = 2;
+                                    } else if( $alternatif['kriteria6'] >= 901000 && $alternatif['kriteria6'] <= 5000000 ) {
+                                        $x6 = 3;
+                                    } else if( $alternatif['kriteria6'] >= 100000 && $alternatif['kriteria6'] <= 900000 ) {
+                                        $x6 = 4;
+                                    } else if( $alternatif['kriteria6'] < 100000 ) {
+                                        $x6 = 5;
+                                    }
+            
+                                    // kriteria7 = lama peluncuran = x7
+                                    if( $alternatif['kriteria7'] > 120 ) {
+                                        $x7 = 5;
+                                    } else if( $alternatif['kriteria7'] >= 91 && $alternatif['kriteria7'] <= 120 ) {
+                                        $x7 = 4;
+                                    } else if( $alternatif['kriteria7'] >= 61 && $alternatif['kriteria7'] <= 96 ) {
+                                        $x7 = 3;
+                                    } else if( $alternatif['kriteria7'] >= 24 && $alternatif['kriteria7'] <= 60 ) {
+                                        $x7 = 2;
+                                    } else if( $alternatif['kriteria7'] < 24 ) {
+                                        $x7 = 1;
+                                    }
+            
+            
+                                    echo "
+                                        <tr>
+                                            <td>". $alternatif['no_alternatif'] ."</td>
+                                            <td>". $alternatif['kriteria1'] ."</td>
+                                            <td>". $x1 ."</td>
+                                            <td>". $x2 ."</td>
+                                            <td>". $x3 ."</td>
+                                            <td>". $x4 ."</td>
+                                            <td>". $x5 ."</td>
+                                            <td>". $x6 ."</td>
+                                            <td>". $x7 ."</td>
+                                        </tr>
+                                    ";
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- matriks X -->
+
+
+    <!-- tombol lihat hasil -->
+    <br><br>
     <div class="lihat-hasil">
         <div class="container">
             <div class="row">
@@ -56,186 +200,13 @@
     </div>
     <!-- tombol lihat hasil -->
 
-    <div class="container">
-        <table>
-            <thead>
-                <tr>
-                    <th>Alternatif</th>
-                    <th>Manajer Investasi</th>
-                    <th>C1</th>
-                    <th>C2</th>
-                    <th>C3</th>
-                    <th>C4</th>
-                    <th>C5</th>
-                    <th>C6</th>
-                    <th>C7</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    foreach($alternatifs as $alternatif) {
-                        // kriteria1 = manajer investasi
-                        if( $alternatif['kriteria1'] == "Sucor Asset Management" || $alternatif['kriteria1'] == "Trimegah Asset Management" ) {
-                            $kriteria1 = 5;
-                        } else if( $alternatif['kriteria1'] == "Asset Management Sinarmas" || $alternatif['kriteria1'] == "Eastpring Investments" ) {
-                            $kriteria1 = 4;
-                        } else {
-                            $kriteria1 = 3;
-                        }
 
-                        // kriteria2 = total AUM
-                        if( $alternatif['kriteria2'] > 1 ) {
-                            $kriteria2 = 5;
-                        } else if( $alternatif['kriteria2'] <= 1 ) {
-                            $kriteria2 = 4;
-                        } else {
-                            $kriteria2 = 3;
-                        }
-
-                        // kriteria3 = CAGR 1 tahun
-                        if( $alternatif['kriteria3'] > 8 ) {
-                            $kriteria3 = 5;
-                        } else if( $alternatif['kriteria3'] >= 6.01 && $alternatif['kriteria3'] <= 8 ) {
-                            $kriteria3 = 4;
-                        } else if( $alternatif['kriteria3'] >= 5.01 && $alternatif['kriteria3'] <= 6 ) {
-                            $kriteria3 = 3;
-                        } else if( $alternatif['kriteria3'] >= 3 && $alternatif['kriteria3'] <= 5 ) {
-                            $kriteria3 = 2;
-                        } else if( $alternatif['kriteria3'] < 3 ) {
-                            $kriteria3 = 1;
-                        }
-
-                        // kriteria4 = dropdown 1 tahun
-                        if( $alternatif['kriteria4'] > 5 ) {
-                            $kriteria4 = 1;
-                        } else if( $alternatif['kriteria4'] >= 3.01 && $alternatif['kriteria4'] <= 5 ) {
-                            $kriteria4 = 2;
-                        } else if( $alternatif['kriteria4'] >= 2.01 && $alternatif['kriteria4'] <= 3 ) {
-                            $kriteria4 = 3;
-                        } else if( $alternatif['kriteria4'] >= 1 && $alternatif['kriteria4'] <= 2 ) {
-                            $kriteria4 = 4;
-                        } else if( $alternatif['kriteria4'] < 1 ) {
-                            $kriteria4 = 5;
-                        }
-
-                        // kriteria5 = expense ratio
-                        if( $alternatif['kriteria5'] > 2 ) {
-                            $kriteria5 = 1;
-                        } else if( $alternatif['kriteria5'] >= 1.51 && $alternatif['kriteria5'] <= 2 ) {
-                            $kriteria5 = 2;
-                        } else if( $alternatif['kriteria5'] >= 1.01 && $alternatif['kriteria5'] <= 1.5 ) {
-                            $kriteria5 = 3;
-                        } else if( $alternatif['kriteria5'] >= 0.5 && $alternatif['kriteria5'] <= 1 ) {
-                            $kriteria5 = 4;
-                        } else if( $alternatif['kriteria5'] < 0.5 ) {
-                            $kriteria5 = 5;
-                        }
-
-                        // kriteria6 = minimal pembelian
-                        if( $alternatif['kriteria6'] > 5000000 ) {
-                            $kriteria6 = 2;
-                        } else if( $alternatif['kriteria6'] >= 901000 && $alternatif['kriteria6'] <= 5000000 ) {
-                            $kriteria6 = 3;
-                        } else if( $alternatif['kriteria6'] >= 100000 && $alternatif['kriteria6'] <= 900000 ) {
-                            $kriteria6 = 4;
-                        } else if( $alternatif['kriteria6'] < 100000 ) {
-                            $kriteria6 = 5;
-                        }
-
-                        // kriteria7 = lama peluncuran
-                        if( $alternatif['kriteria7'] > 120 ) {
-                            $kriteria7 = 5;
-                        } else if( $alternatif['kriteria7'] >= 91 && $alternatif['kriteria7'] <= 120 ) {
-                            $kriteria7 = 4;
-                        } else if( $alternatif['kriteria7'] >= 61 && $alternatif['kriteria7'] <= 96 ) {
-                            $kriteria7 = 3;
-                        } else if( $alternatif['kriteria7'] >= 24 && $alternatif['kriteria7'] <= 60 ) {
-                            $kriteria7 = 2;
-                        } else if( $alternatif['kriteria7'] < 24 ) {
-                            $kriteria7 = 1;
-                        }
-
-
-                        echo "
-                            <tr>
-                                <td>". $alternatif['no_alternatif'] ."</td>
-                                <td>". $alternatif['kriteria1'] ."</td>
-                                <td>". $kriteria1 ."</td>
-                                <td>". $kriteria2 ."</td>
-                                <td>". $kriteria3 ."</td>
-                                <td>". $kriteria4 ."</td>
-                                <td>". $kriteria5 ."</td>
-                                <td>". $kriteria6 ."</td>
-                                <td>". $kriteria7 ."</td>
-                            </tr>
-                        ";
-                        echo "<br>";
-                    }
-                ?>
-            </tbody>
-        </table>
-        <?php
-            foreach($alternatifs as $alternatif) {
-                // echo $alternatif['no_alternatif'];
-                // echo " = ";
-                // echo $alternatif['kriteria1'];
-                // echo " = ";
-
-
-                // kriteria1
-                if( $alternatif['kriteria1'] == "Sucor Asset Management" || $alternatif['kriteria1'] == "Trimegah Asset Management" ) {
-                    $kriteria1 = 5;
-                } else if( $alternatif['kriteria1'] == "Asset Management Sinarmas" || $alternatif['kriteria1'] == "Eastpring Investments" ) {
-                    $kriteria1 = 4;
-                } else {
-                    $kriteria1 = 3;
-                }
-
-                // kriteria2
-                // if( $alternatif['kriteria1'] == "Sucor Asset Management" || $alternatif['kriteria1'] == "Trimegah Asset Management" ) {
-                //     $kriteria1 = 5;
-                // } else if( $alternatif['kriteria1'] == "Asset Management Sinarmas" || $alternatif['kriteria1'] == "Eastpring Investments" ) {
-                //     $kriteria1 = 4;
-                // } else {
-                //     $kriteria1 = 3;
-                // }
-                
-
-                // echo "[" . $kriteriaA1 = $kriteria1 . "]";
-                // echo " = ";
-                // echo $alternatif['kriteria2'];
-
-                // echo "<br>";
-                
-                // tabel
-                // echo "
-                //     <table>
-                //         <thead>
-                //             <tr>
-                //                 <th>Alternatif</th>
-                //                 <th>Manajer Investasi</th>
-                //                 <th>Manajer Investasi</th>
-                //                 <th>Total AUM</th>
-                //             </tr>
-                //         </thead>
-                //         <tbody>
-                //             <tr>
-                //                 <td>". $alternatif['no_alternatif'] ."</td>
-                //                 <td>". $alternatif['kriteria1'] ."</td>
-                //                 <td>". $kriteriaA1 = $kriteria1 ."</td>
-                //                 <td>". $alternatif['kriteria2'] ."</td>
-                //             </tr>
-                //         </tbody>
-                //     </table>
-                // ";
-            }
-
-            
-            
-        ?>
-    </div>
-
-
+    <!-- footer -->
+    <br><br>
+    <footer class="grey darken-2 white-text center">
+        <p>&copy; Riki Widiantoro</p>
+    </footer>
+    <!-- footer -->
 
     <!--JavaScript at end of body for optimized loading-->
     <!-- <script type="text/javascript" src="js/materialize.min.js"></script> -->
