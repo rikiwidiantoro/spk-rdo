d2b2c20f7b1ff963e4804ecfd683ddd75ffd038b
Riki-Toro <rikitoro22@gmail.com>
2022-05-15 22:04:37 +0700

    login

--- crud/tambahKriteria.php
+++ crud/tambahKriteria.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Tambah Data Kriteria</title>
-</head>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Tambah Data Kriteria</title>
+    </head>
 
-<body>
+    <body>
 
-    <!-- navbar -->
-    <div class="navbar-fixed">
-        <nav class="grey darken-2">
-            <div class="nav-wrapper container">
-                <a href="../index.php"><i class="material-icons left">keyboard_backspace</i>Kembali</a>
-                <a href="#" class="brand-logo center">SPK Reksa Dana Obligasi</a>
-            </div>
-        </nav>
-    </div>
-    <!-- navbar -->
-
-
-    <!-- form -->
-    <!-- form -->
+        <!-- navbar -->
+        <div class="navbar-fixed">
+            <nav class="grey darken-2">
+                <div class="nav-wrapper container">
+                    <a href="../index.php"><i class="material-icons left">keyboard_backspace</i>Kembali</a>
+                    <a href="#" class="brand-logo center">SPK Reksa Dana Obligasi</a>
+                </div>
+            </nav>
+        </div>
+        <!-- navbar -->
 
 
-    <!--JavaScript at end of body for optimized loading-->
-    <!-- <script type="text/javascript" src="js/materialize.min.js"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-</body>
+        <!-- form -->
+        <!-- form -->
+
+
+        <!--JavaScript at end of body for optimized loading-->
+        <!-- <script type="text/javascript" src="js/materialize.min.js"></script> -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </body>
 </html>
---
+++ login.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Masuk</title>
+        <style>
+            .container {
+                /* border: 1px solid black; */
+                margin-top: 110px;
+                padding-bottom: 10px;
+                border-radius: 7px;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <div class="container grey lighten-2">
+            <div class="row">
+                <div class="col m8 offset-m2">
+                    <h2 class="center">SPK Reksa Dana Obligasi</h2>
+                    <br>
+                    <form action="">
+                        <div class="row">
+                            <div class="col s3">
+                                <h5>Username : </h5>
+                            </div>
+                            <div class="col s9">
+                                <input id="text" type="text">
+                            </div>
+                            <!-- <div class="input-field col s8">
+                                <input id="text" type="text" class="validate">
+                                <label for="text">Username</label>
+                            </div> -->
+                        </div>
+                        <div class="row">
+                            <div class="col s3">
+                                <h5>Password : </h5>
+                            </div>
+                            <div class="col s9">
+                                <input id="password" type="password">
+                            </div>
+                            <!-- <div class="input-field col s12">
+                                <input id="password" type="password" class="validate">
+                                <label for="password">Password</label>
+                            </div> -->
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col s3 offset-s5">
+                                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                                    <i class="material-icons right">send</i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col m4 offset-m2">
+                    <p>username : rikiwidiantoro</p>
+                </div>
+                <div class="col m4">
+                    <p>password : rikiwidiantoro</p>
+                </div>
+            </div>
+        </div>
+
+
+        <!--JavaScript at end of body for optimized loading-->
+        <!-- <script type="text/javascript" src="js/materialize.min.js"></script> -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </body>
+</html>
